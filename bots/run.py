90630155f018b630a48956bee3b2413736efd325
Abdullah Sinanović <budad-97@hotmail.com>
2023-11-30 17:30:13 +0100

    Everything up to mobile verification is done

--- bots/run.py
+++ bots/run.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -50,10 +51,14 @@ class Automation(webdriver.Chrome):
         executable_path = ChromeDriverManager().install()
         self.service = Service(executable_path)
         self.options = Options()
+        #self.options.add_argument('--headless')  # Dodajte ovu opciju za headless režim
+        #self.options.add_argument('--disable-gpu')  # Opciono, može pomoći sa headless režimom
         self.options.add_argument('--detach=false')
         self.proxy=proxy
         self.user_agent = None
+        self.wait=WebDriverWait(self, 15)
         prefs = {"profile.password_manager_enabled": False, "credentials_enable_service": False, "useAutomationExtension": False}
+        self.options.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.options.add_experimental_option("prefs", prefs)
         self.options.add_experimental_option("useAutomationExtension", False)
         self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -64,6 +69,12 @@ class Automation(webdriver.Chrome):
         self.options.add_argument('--ignore-ssl-errors=yes')
         self.options.add_argument('--ignore-certificate-errors')
         super(Automation, self).__init__(options=self.options, service=self.service)
+        self.position=self.get_window_position()
+        self.new_position = (self.position['x'] + 500, self.position['y'])
+        self.set_window_position(*self.new_position)
+        self.first_name = None
+        self.last_name = None
+
         
     def waiting(self):
         time.sleep(random.randint(1,3))
@@ -71,9 +82,9 @@ class Automation(webdriver.Chrome):
     def open_window(self):
         if self.proxy:
             self.options.add_argument(f'--proxy-server={self.proxy}')
-        self.user_agent = UserAgent(fallback="Mozilla/5.0 (Macintosh; Intel Mac OS X10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36").random
-        self.options.add_argument(f"user-agent={self.user_agent}")
-        print(f"user agent: {self.user_agent}")
+        """self.user_agent = UserAgent(fallback="Mozilla/5.0 (Macintosh; Intel Mac OS X10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36").random
+        self.options.add_argument(f"user-agent={self.user_agent}")"""
+        #print(f"user agent: {self.user_agent}")
         self.random_url = random.choice(SITE_LIST)
         print(self.random_url)
         super(Automation, self).get(f"{self.random_url}")
@@ -86,7 +97,7 @@ class Automation(webdriver.Chrome):
         try:
             first_names= [line.strip() for line in first_name_file]
             random_name=random.sample(first_names, 1)
-            print(random_name)
+            return random_name
         except:
             print("Name not found, check your code or names list")
             self.quit()
@@ -111,26 +122,192 @@ class Automation(webdriver.Chrome):
             print("Password generation failed!")
             self.quit()
 
-    def get_user_data(self):
-        first_name=self.get_first_name()
-        last_name=self.get_last_name()
-        password =self.generate_password()
-        birthday=str(random.randint(1,12)) + "/" + str(random.randint(1,28)) + "/" +  str(random.randint(1980,1999))
-        print(str(first_name) + "\t" + str(last_name) + "\t" + str(password) + '\t' + str(birthday))
+    def get_birthday(self):
+        month=random.randint(1,12)
+        day=random.randint(1,28)
+        year=random.randint(1980,2004)
+        birthday={'month':month,
+                  'day': day,
+                  'year':year}
+        return birthday
 
     def go_to_acc_creation(self):
-        random_way=random.randint(1,4)
+        random_way=random.randint(1,3)
         print(f"random way choose way number: {random_way}")
 
         if random_way == 1:
+            print(f"proxy is: {self.proxy}")
             print("Way Number 1")
-            self.get('https://support.google.com/accounts/answer/27441?hl=en')
+            try:
+                self.get('https://support.google.com/accounts/answer/27441?hl=en')
+                target_element = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="gb"]/div[2]/div[3]/div[1]/a')))
+                target_element.click()
+                create_new=self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div[2]/div/div[2]/div/div/div[1]/div/button')))   
+                create_new.click()
+                selector=self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div[2]/div/div[2]/div/div/div[2]/div/ul/li[1]')))    
+                selector.click()  
+                print("Way 1 acc creation page successfully open")
+            except:
+                print("Page not opened, task failed - program closing...!")
+
         elif random_way==2:
+            print(f"proxy is: {self.proxy}")
             print("Way Number 2")
-            self.get("https://accounts.google.com")
+            try:
+                self.get("https://accounts.google.com")
+                target_element2 = self.wait.until(EC.presence_of_element_located((By.XPATH, "//button[@class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-dgl2Hf ksBjEc lKxP2d LQeN7 FliLIb uRo0Xe TrZEUc Xf9GD']")))
+                target_element2.click()
+                for_personal_use = self.wait.until(EC.presence_of_element_located((By.XPATH, "//li[@class='G3hhxb VfPpkd-StrnGf-rymPhb-ibnC6b']"))).click()
+                print("Way 2 acc creation page successfully open")
+            except:
+                print("Page not opened, task failed - program closing...!")
+
+
         elif random_way==3:
+            print(f"proxy is: {self.proxy}")
             print("Way Number 3")
-            self.get('https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp')
-        elif random_way==4:
-            print("Way Number 4")
-            self.get('https://support.google.com/mail/answer/56256?hl=en')
+            try:
+                self.get('https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp')
+                print("Way 3 acc creation page successfully open")
+            except:
+                print("Page not opened, task failed - program closing...!")
+
+
+    def pass_first_last_name(self):
+        first_name_tag = self.wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='firstName']")))    #//*[@id="firstName"] #//*[@id="firstName"]
+        
+        if first_name_tag:
+            first_name_tag.clear()
+            self.first_name = self.get_first_name()
+            print(self.first_name)
+            first_name_tag.send_keys(self.first_name)
+        else:
+            print("Element nije pronađen.")
+
+        time.sleep(1)
+        last_name_tag=self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="lastName"]')))
+        if last_name_tag:
+            last_name_tag.clear()
+            time.sleep(1)
+            self.last_name=self.get_last_name()
+            print(self.last_name)
+            last_name_tag.send_keys(self.last_name)
+        else:
+            print("Element nije pronađen.")
+
+        
+        next_button = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="collectNameNext"]/div/button')))
+        if next_button:
+            next_button.click()
+        
+
+
+    def pass_birthday_details(self):
+        birthday_details=self.get_birthday()
+        month_tag=self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="month"]')))
+        if month_tag:
+            desired_month=Select(month_tag)
+            desired_month.select_by_value(str(birthday_details["month"]))
+        else:
+            print("Month element not located")
+
+        day_tag=self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="day"]')))
+        if day_tag:
+            day_tag.send_keys(birthday_details["day"])
+        else:
+            print("Day Input field element not located")
+
+        year_tag=self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="year"]')))
+        if year_tag:
+            year_tag.send_keys(birthday_details["year"])
+        else:
+            print("Year Input field element not located")
+
+        gender = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="gender"]')))
+        if gender:
+            desired_gender=Select(gender)
+            desired_gender.select_by_value(str(random.randint(1,2)))
+        else:
+            print("Gender field error")
+
+        time.sleep(1)
+        next_button = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="birthdaygenderNext"]')))
+        if next_button:
+            next_button.click()
+        else:
+            print("Problem with confirming button")
+
+        time.sleep(1)
+
+    def get_email_adress(self):
+        if self.first_name is not None and self.last_name is not None:
+            digits = random.randint(1, 1000)
+            gmail_adress = f"{self.first_name[0]}{self.last_name[0]}.{digits}"
+            print(gmail_adress)
+        else:
+            print("Error unpacking name data")
+
+
+        attempts = 0
+        while attempts < 3:
+            try:
+                print("Attempt:", attempts + 1)
+                make_username = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/span/div[3]/div'))) 
+                make_username.click()
+                break 
+            except Exception as e:
+                print(f"Error clicking 'Make Username' button: {e}")
+                attempts += 1
+
+        if attempts == 3:
+            print("Failed to click 'Make my own username' button after 3 attempts.")
+
+
+        try:
+            new_username_field=self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[2]/div[1]/div/div[1]/div/div[1]/input')))
+            if new_username_field:
+                new_username_field.clear()
+                new_username_field.send_keys(gmail_adress)
+        except:
+            print("Error entering new Gmail username")
+
+        try:
+            next_button = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="next"]/div/button')))
+            next_button.click()
+        except:
+            print("Error finding next button")
+        time.sleep(3)
+
+    def set_password(self):
+        print("Setting password...")
+        characters = string.ascii_letters + string.digits  # slova + brojevi
+        password_length = random.randint(8, 12)
+        password = ''.join(random.choice(characters) for _ in range(password_length))
+        print(f"Password is: {password}")
+
+        try:
+            password_field=self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="passwd"]/div[1]/div/div[1]/input')))
+            password_field.clear()
+            password_field.send_keys(password)
+        except:
+            print("Error setting password")
+
+        try:
+            confirm_field=self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="confirm-passwd"]/div[1]/div/div[1]/input')))
+            confirm_field.clear()
+            confirm_field.send_keys(password)
+        except:
+            print("Error setting password")
+
+        time.sleep(2)
+
+        try:
+            next_button=self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="createpasswordNext"]/div/button')))
+            next_button.click()
+        except:
+            print("Error setting password")
+
+        print("Password is set")
+        time.sleep(4)
+
+        
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import requests, random
 from bots.run import Automation
 
 with open("valid_proxy.txt", 'r') as f:
@@ -7,11 +6,8 @@ with open("valid_proxy.txt", 'r') as f:
 for index,proxy in enumerate(proxies[:1]):
     with Automation(proxy=proxy) as bot:
         bot.open_window()
-        bot.waiting()
-        """print(f"Using proxy {index+1} - proxy: {proxy}")
-        bot.waiting()
-        bot.get_first_name()
-        bot.get_last_name()
-        bot.generate_password()
-        bot.get_user_data()"""
         bot.go_to_acc_creation()
+        bot.pass_first_last_name()
+        bot.pass_birthday_details()
+        bot.get_email_adress()
+        bot.set_password()
